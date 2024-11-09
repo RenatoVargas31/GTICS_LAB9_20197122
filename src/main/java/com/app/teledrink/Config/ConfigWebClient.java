79b8a35080f680e9e22b9651383e27cea24e0bf6
RenatoVargas31 <137341256+RenatoVargas31@users.noreply.github.com>
2024-11-09 12:03:47 -0500

    Base Commit

---
+++ src/main/java/com/app/teledrink/Config/ConfigWebClient.java
@@ -0,0 +1,15 @@
+package com.app.teledrink.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ConfigWebClient {
+    String baseUrl = "https://www.thecocktaildb.com/api/json/v1/1";
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().baseUrl(baseUrl).build();
+    }
+}
