ab82c8532b8991fbc28fa779932fb6bee35ef782
RenatoVargas31 <137341256+RenatoVargas31@users.noreply.github.com>
2024-11-09 11:53:27 -0500

    first commit

---
+++ src/main/java/com/app/teledrink/TeledrinkApplication.java
@@ -0,0 +1,13 @@
+package com.app.teledrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeledrinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeledrinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/teledrink/TeledrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.teledrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeledrinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
